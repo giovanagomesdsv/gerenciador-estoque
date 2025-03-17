6955041868af72b5881ab726ef3ac1dd811ea061
Giovana Gomes <g.gomesdsv@gmail.com>
2025-03-17 12:27:43 -0300

    conectando com o banco de dados

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "bc";
+$hostname = "10.67.22.216";
+$banco = "bd_tcc_etim_123_g2";
+$usuario = "us_etim_123_g2";
+$senha = "ec0623";
 
 $conn = new mysqli($servidor, $usuario, $senha, $banco);
 
